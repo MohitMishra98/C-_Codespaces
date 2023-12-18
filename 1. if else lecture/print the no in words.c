bceeb7884276d63edceacab387d0934b2ab8a9d7
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2023-12-18 13:55:52 +0000

    a

---
+++ 1. if else lecture/print the no in words.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void main() {
+    int a;
+    printf("Enter a number :");
+    scanf("%d",&a);
+
+    if(a == 1) {
+        printf("1");
+    }
+    else if(a == 2) {
+        printf("2");
+    }
+    else if(a == 3) {
+        printf("3");
+    }
+    else if(a == 4) {
+        printf("4");
+    }
+    else if(a == 5) {
+        printf("5");
+    }
+    else{
+        printf("Invalid Number");
+    }
+
+    printf("\n\n\n\n\n\n\n");
+    
+}
