4361165dc4655da3399c1c062d476f92da12b837
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2024-03-06 15:14:44 +0000

    added solutions

---
+++ SigLabQuestions/Ques20.c
@@ -0,0 +1,77 @@
+//WAP to get a number from user and check it is armstrong number or not
+
+//An Armstrong number is a number that is equal to the sum of its own digits each raised to the power of the number of digits.
+//For example, consider the number 153. It has 3 digits, and if we cube each digit and then sum them, we get the number itself:
+//1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+#include<stdio.h>
+
+void main(){
+    int a=0,n,r,b,sum=0,temp,rnd;
+
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+
+    b=n;
+
+    //loop to get numbers of digits
+    while(n!=0){
+        r=n%10;
+        n=n/10;
+        a++;
+    }
+
+    //reset the variable values
+    r=0;
+    n=b;
+
+    //
+    while(n!=0){
+        r=n%10;
+        temp=getpow(r,a);
+        sum=sum+temp;
+        n=n/10;
+    }
+
+    //printf("%d",sum);
+
+    if(b==sum){
+        printf("%d is a armstrong number",b);
+    }
+    else {
+        printf("%d is not a armstrong number",b);
+    }
+}
+
+//function to get power of a number
+int getpow(int s,int k){
+    int rnd=1,num=1;
+    while(rnd<=k){
+        num = num*s;
+        rnd++;
+    }
+    return num;
+}
+
+
+
+
+
+/*
+1.get the number from user
+2.check the number of digits
+3.extract all the digits
+4.add all the numbers to the power of number of digits
+5.if original number is same as the new number then it is armstrong number
+
+
+
+
+
+void main(){
+    int a;
+    a=pow(3,4);
+    printf("%d",a);
+}
+*/
