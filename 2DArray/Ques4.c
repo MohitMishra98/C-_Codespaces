b53f5963bb1b463fa34a8120badcecd471bcd33a
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2024-04-21 07:15:54 +0000

    added code

---
+++ 2DArray/Ques4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main(){
+    int a[3][3],i,j,max;
+
+    printf("Enter an array\n");
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("Enter value for index A%d%d : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    max = a[0][0];
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            if(max<a[i][j]){
+                max = a[i][j];
+            }
+        }
+    }
+
+    printf("Max valued element is : %d",max);
+}
