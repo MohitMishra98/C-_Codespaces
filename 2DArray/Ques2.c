d7236750540f53015bc27d0cbf4dcc97cd65628a
Mohit Mishra <150663253+MohitMishra98@users.noreply.github.com>
2024-04-21 07:00:35 +0000

    added code

---
+++ 2DArray/Ques2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+    int a[3][3],i,j,sum=0;
+
+    printf("Enter an array : \n");
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("Enter value for index A%d%d : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            sum = sum + a[i][j];
+        }
+    }
+
+    printf("Sum of all elements is : %d",sum);
+}
